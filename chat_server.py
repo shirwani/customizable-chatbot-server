b6e713c4d2aa908af713934ab9eb080eda84185a
Zaki Shirwani <zakishirwani@alumni.purdue.edu>
2026-02-17 23:20:59 -0500

    .

--- chat_server.py
+++ chat_server.py
@@ -77,4 +77,4 @@ def home():
 
 if __name__ == "__main__":
     # Bind on all interfaces so the client (running on a different port) can reach it
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8001, debug=True)
